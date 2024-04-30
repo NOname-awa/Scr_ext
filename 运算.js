d1769620b70c3f0dc20e366cdaaea6cdd0c4b58d
NOname-awa <108421637@qq.com>
2024-05-01 01:15:33 +0800

    把 var 变量改为 let 变量

--- 运算.js
+++ 运算.js
@@ -5198,15 +5198,15 @@
                 ), 0
             );
 
-            var r1 = parseInt(c1.substring(1, 3), 16);
-            var g1 = parseInt(c1.substring(3, 5), 16);
-            var b1 = parseInt(c1.substring(5, 7), 16);
-            var r2 = parseInt(c2.substring(1, 3), 16);
-            var g2 = parseInt(c2.substring(3, 5), 16);
-            var b2 = parseInt(c2.substring(5, 7), 16);
-            var r = Math.round(r1 * (1 - ratio) + r2 * ratio);
-            var g = Math.round(g1 * (1 - ratio) + g2 * ratio);
-            var b = Math.round(b1 * (1 - ratio) + b2 * ratio);
+            let r1 = parseInt(c1.substring(1, 3), 16);
+            let g1 = parseInt(c1.substring(3, 5), 16);
+            let b1 = parseInt(c1.substring(5, 7), 16);
+            let r2 = parseInt(c2.substring(1, 3), 16);
+            let g2 = parseInt(c2.substring(3, 5), 16);
+            let b2 = parseInt(c2.substring(5, 7), 16);
+            let r = Math.round(r1 * (1 - ratio) + r2 * ratio);
+            let g = Math.round(g1 * (1 - ratio) + g2 * ratio);
+            let b = Math.round(b1 * (1 - ratio) + b2 * ratio);
             r = ('0' + (r || 0).toString(16)).slice(-2);
             g = ('0' + (g || 0).toString(16)).slice(-2);
             b = ('0' + (b || 0).toString(16)).slice(-2);
